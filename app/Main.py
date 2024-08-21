ff27be51e6c226414448a4d90c95c770586c89b2
Beatriz Dorin <beatrizdorin@gmail.com>
2024-08-21 20:58:16 -0300

    Update Main.py

--- app/Main.py
+++ app/Main.py
@@ -5,8 +5,12 @@ print ("Testando o Projeto")
 
 from Cliente import Cliente
 
-c1= Cliente('João', '114444-2222')
+from Conta import Conta
 
-print (c1)
-print (c1.nome, ' e ', c1.telefone)
+c1= Cliente('João', '114444-2222')
+conta=Conta(c1.nome,6565,0)
+#pra puxar o nome novamente, precisa ser "c1.get_nome()"
+
+print (conta.titular," Numero: ",conta.numero,"Seu Saldo: ", conta.saldo)
+
 
