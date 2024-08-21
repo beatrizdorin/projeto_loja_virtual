3cce98aca423d9ee9b3a9180a43ff988982feb53
Beatriz Dorin <beatrizdorin@gmail.com>
2024-08-21 20:58:10 -0300

    get e set
    Criação da classe conta.
    Encapsulamento.
    Adicionando get e set na classe conta.

---
+++ app/Conta.py
@@ -0,0 +1,14 @@
+class Conta:
+    def __init__(self, titular, numero, saldo):
+        self.saldo=0
+        self.numero=numero
+        self.titular=titular
+
+        def get_saldo(self):
+            return self._saldo
+
+        def set_saldo(self, saldo):
+            if (saldo<0):
+                print('O saldo não pode ser negativo')
+            else:
+                self._saldo = saldo
