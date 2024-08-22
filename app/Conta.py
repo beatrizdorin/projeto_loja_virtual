76a5cf90c54929e7b41e7068faa3b0e79010600f
Beatriz Dorin <beatrizdorin@gmail.com>
2024-08-22 20:10:40 -0300

    Update Conta.py

--- app/Conta.py
+++ app/Conta.py
@@ -1,8 +1,8 @@
 class Conta:
     def __init__(self, titular, numero, saldo):
-        self.saldo=0
-        self.numero=numero
-        self.titular=titular
+        self._saldo=0
+        self._numero=numero
+        self._titular=titular
 
         def get_saldo(self):
             return self._saldo
@@ -12,3 +12,16 @@ class Conta:
                 print('O saldo não pode ser negativo')
             else:
                 self._saldo = saldo
+
+    def saque(self, valor):
+        if (self._saldo>=valor):
+            self._saldo-=valor
+            print("Saque realizado com sucesso")
+        else:
+            print('Saldo Insuficiente')
+
+    def deposita(self,valor):
+        self._saldo+=valor
+
+    def extrato(self):
+        print('Cliente: ',self._titular, "Saldo Atual: ",self._saldo)
